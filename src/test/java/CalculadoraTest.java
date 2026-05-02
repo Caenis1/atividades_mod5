3c647415832a48650656f7f8d7daa3fa770966d3
Caenis1 <carlossocial1479@gmail.com>
2026-05-02 15:43:11 -0300

    commit

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,3 +1,40 @@
-public class CalculadoraTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import atividades_mod5.Calculadora;
+
+class CalculadoraTest {
+
+    Calculadora calc = new Calculadora();
+
+    @Test
+    void deveSomarCorretamente() {
+        assertEquals(5, calc.adicionar(2, 3));
+    }
+
+    @Test
+    void deveSubtrairCorretamente() {
+        assertEquals(1, calc.subtrair(3, 2));
+    }
+
+    @Test
+    void deveMultiplicarCorretamente() {
+        assertEquals(6, calc.multiplicar(2, 3));
+    }
+
+    @Test
+    void deveDividirCorretamente() {
+        assertEquals(2, calc.dividir(4, 2));
+    }
+
+    @Test
+    void deveLancarExcecaoAoDividirPorZero() {
+        assertThrows(ArithmeticException.class, () -> calc.dividir(4, 0));
+    }
+
+    @Test
+    void deveFuncionarComNumerosNegativos() {
+        assertEquals(-6, calc.multiplicar(-2, 3));
+    }
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,3 +1,29 @@
-public class FibonacciTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
+
+    Fibonacci fib = new Fibonacci();
+
+    @Test
+    void deveRetornarZeroParaNZero() {
+        assertEquals(0, fib.calcular(0));
+    }
+
+    @Test
+    void deveRetornarUmParaNUm() {
+        assertEquals(1, fib.calcular(1));
+    }
+
+    @Test
+    void deveCalcularValorCorretoParaNumeroMedio() {
+        assertEquals(5, fib.calcular(5));
+        assertEquals(13, fib.calcular(7));
+    }
+
+    @Test
+    void deveLancarExcecaoParaNumeroNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> fib.calcular(-1));
+    }
 }
